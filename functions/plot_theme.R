44abab3492cda83ed3991df2682c3ea00c2e4d14
jan_pfaender <janlukas.pfaender@gmail.com>
2025-03-25 17:19:35 +0100

    PUS submission with annonymized repository

--- functions/plot_theme.R
+++ functions/plot_theme.R
@@ -1,13 +1,14 @@
-# set general theme for plots
+# Set general theme for plots
 plot_theme <- theme_minimal(base_size = 12) +
-  theme(# Bold title
+  theme(
+    # Bold title
     plot.title = element_text(face = "bold", size = rel(0.7)),
     # Plain subtitle that is grey
     plot.subtitle = element_text(face = "plain", size = rel(1), color = "grey70"),
-    # Bold legend titles
-    legend.title = element_text(face = "bold"),
+    # Slightly smaller bold legend title
+    legend.title = element_text(face = "bold", size = 10),  # Adjust the size here for a smaller legend title
     # Bold axis titles
-    axis.title = element_text(face = "bold"), 
-    # Bold legend labels
-    legend.text = element_text(face = "bold")
-    )
+    axis.title = element_text(face = "bold"),
+    # Smaller legend text
+    legend.text = element_text(size = 9)  # Adjust the size here for smaller legend text
+  )
